becdecc1733910f85ea647d4cb3e46e1d55fcbce
Ali Abdurrahman <aliabdurrahman25032001@gmail.com>
2017-11-10 07:08:42 +0700

    fix

---
+++ TestAlamofire/TestAlamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAlamofire
+//
+//  Created by Ali Abdurrahman Hidayat on 10/11/17.
+//  Copyright © 2017 Ali Abdurrahman Hidayat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
